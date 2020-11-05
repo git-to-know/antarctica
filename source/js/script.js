a2e66d4bdae50e762092c9832c742598882a614b
git-to-know <irinaudovitsa@yandex.ru>
2020-11-05 22:29:34 +0300

    выполнены все блоки

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,31 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+// pageHeader.classList.remove('page-header--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
+var openButton = document.querySelector('.main-nav__toggle--open');
+var mainNav = document.querySelector('.main-main');
+var closeButton = document.querySelector('.main-nav__toggle--close');
+
+mainNav.classList.remove('main-nav__menu');
+
+openButton.addEventListener('click', function (evt) {
+  // evt.preventDefault();
+  mainNav.classList.remove('main-nav');
+});
+
+closeButton.addEventListener('click', function (evt) {
+  // evt.preventDefault();
+  mainNav.classList.remove('main-nav__menu');
+  mainNav.classList.add('main-nav');
 });
