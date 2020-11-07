b11bc211642e1b05171713edd776df13d7fc6f85
git-to-know <irinaudovitsa@yandex.ru>
2020-11-07 18:30:16 +0300

    проект выполнен

--- build/js/script.js
+++ build/js/script.js
@@ -1,31 +1,17 @@
 'use strict';
-// var pageHeader = document.querySelector('.page-header');
-// var headerToggle = document.querySelector('.page-header__toggle');
 
-// pageHeader.classList.remove('page-header--nojs');
-
-// headerToggle.addEventListener('click', function () {
-//   if (pageHeader.classList.contains('page-header--closed')) {
-//     pageHeader.classList.remove('page-header--closed');
-//     pageHeader.classList.add('page-header--opened');
-//   } else {
-//     pageHeader.classList.add('page-header--closed');
-//     pageHeader.classList.remove('page-header--opened');
-//   }
-// });
 var openButton = document.querySelector('.main-nav__toggle--open');
-var mainNav = document.querySelector('.main-main');
+var mainNav = document.querySelector('.main-nav');
 var closeButton = document.querySelector('.main-nav__toggle--close');
 
-mainNav.classList.remove('main-nav__menu');
+mainNav.classList.remove('main-nav--show')
 
 openButton.addEventListener('click', function (evt) {
   evt.preventDefault();
-  mainNav.classList.remove('main-nav');
+  mainNav.classList.add('main-nav--show');
 });
 
 closeButton.addEventListener('click', function (evt) {
   evt.preventDefault();
-  mainNav.classList.remove('main-nav__menu');
-  mainNav.classList.add('main-nav');
+  mainNav.classList.remove('main-nav--show');
 });
